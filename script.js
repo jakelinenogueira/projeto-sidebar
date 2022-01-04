756f63c6a515f1ce4194888bd1163bbc6d54d8d5
Jakeline Nogueira <jakenogueira94@gmail.com>
2022-01-04 20:26:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleSidebar(){
+  document.querySelector('#sidebar').classList.toggle("sidebar-hide");
+}
+
+
+//document.querySelector('#sidebar');
+//document.querySelector('#sidebar').setAttribute('class', 'sidebar-hide');
